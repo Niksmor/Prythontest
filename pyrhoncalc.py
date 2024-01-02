2fd31843fcb13ef430b681ecc967c9ee04e108f9
Niksmor <145490281+Niksmor@users.noreply.github.com>
2024-01-02 15:24:29 +0300

    Add files via upload

---
+++ pyrhoncalc.py
@@ -0,0 +1,15 @@
+num1 = int(input('Enter first number: '))
+num2 = int(input('Enter second number: '))
+op = input('Enter Operator ')
+
+
+if op == "+":
+    print(num1 + num2)
+elif op == '-':
+    print('The substraction is ', num1-num2)
+elif op == '*':
+    print('The multiplication is ', num1*num2)
+elif op == '/':
+    print('The division is ', abs(num1/num2))
+else:
+    print('Invalid operator ')
